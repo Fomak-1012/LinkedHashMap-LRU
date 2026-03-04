a71e2ed3e68d46608ccc641af5e085357a40874e
Fomak <fomalhaut_1012@sjtu.edu.cn>
2026-03-04 18:40:09 +0800

    feat : to finish double_list

--- lru/lru.hpp
+++ lru/lru.hpp
@@ -27,15 +27,47 @@ public:
 	 * elements
 	 * add whatever you want
 	 */
+	struct node {
+		T* val;
+		node *prev, *next;
+		node() = delete;
+		node(const T& v, node* p = nullptr, node* n = nullptr): val(new T(v)), prev(p), next(n) {}
+		~node() { delete val; }
+	};
 
 	// --------------------------
 	/**
 	 * the follows are constructors and destructors
 	 * you can also add some if needed.
 	 */
-	double_list() {}
-	double_list(const double_list<T> &other) {}
-	~double_list() {}
+	double_list() {
+		head = new node();
+		tail = new node();
+		head->next = tail;
+		tail->prev = head;
+	}
+	double_list(const double_list<T> &other) {
+		head = new node();
+		tail = new node();
+		head->next = tail;
+		tail->prev = head;
+
+		node* cur = other.head->next;
+		while (cur != other.tail) {
+			insert_tail(*cur->val);
+			cur = cur->next;
+		}
+	}
+	~double_list() {
+		node* cur = head->next;
+		while (cur != tail) {
+			node* tmp = cur;
+			cur = cur->next;
+			delete tmp;
+		}
+		delete head;
+		delete tail;
+	}
 
 	class iterator {
 	public:
@@ -43,55 +75,93 @@ public:
 		 * elements
 		 * add whatever you want
 		 */
+		node *p;
 		// --------------------------
 		/**
 		 * the follows are constructors and destructors
 		 * you can also add some if needed.
 		 */
-		iterator() {}
-		iterator(const iterator &t) {}
+		iterator() : p(nullptr) {}
+		iterator(const iterator &t) : p(t.p) {}
 		~iterator() {}
 		
 		/**
 		 * iter++
 		 */
-		iterator operator++(int) {}
+		iterator operator++(int) {
+			if(p->next == nullptr)
+				throw "It's the end!";
+			iterator tmp(*this);
+			p = p->next;
+			return tmp;
+		}
 		/**
 		 * ++iter
 		 */
-		iterator &operator++() {}
+		iterator &operator++() {
+			if(p->next == nullptr)
+				throw "It's the end!";
+			p = p->next;
+			return *this;
+		}
 		/**
 		 * iter--
 		 */
-		iterator operator--(int) {}
+		iterator operator--(int) {
+			if(p->prev->prev == nullptr)
+				throw "It's the beginning!";
+			iterator tmp(*this);
+			p = p->prev;
+			return tmp;
+		}
 		/**
 		 * --iter
 		 */
-		iterator &operator--() {}
+		iterator &operator--() {
+			if(p->prev->prev == nullptr)
+				throw "It's the beginning!";
+			p = p->prev;
+			return *this;
+		}
 		
 		/**
 		 * if the iter didn't point to a value
 		 * throw " invalid"
 		 */
-		T &operator*() const {}
+		T &operator*() const {
+			if(p == nullptr || p->prev == nullptr || p->next == nullptr || p->val == nullptr)
+				throw " invalid";
+			return *p->val;
+		}
 		
 		/**
 		 * other operation
 		 */
-		T *operator->() const noexcept {}
-		bool operator==(const iterator &rhs) const {}
-		bool operator!=(const iterator &rhs) const {}
+		T *operator->() const noexcept {
+			return p->val;
+		}
+		bool operator==(const iterator &rhs) const {
+			return p == rhs.p;
+		}
+		bool operator!=(const iterator &rhs) const {
+			return p != rhs.p;
+		}
 	};
 	/**
 	 * return an iterator to the beginning
 	 */
-	iterator begin() {}
+	iterator begin() const {
+		return iterator(head->next);
+	}
 	/**
 	 * return an iterator to the ending
 	 * in fact, it returns the iterator point to nothing,
 	 * just after the last element.
 	 */
-	iterator end() {}
+	// 需支持 end()--
+	iterator end() const {
+		return iterator(tail);
+	}
 	/**
 	 * if the iter didn't point to anything, do nothing,
 	 * otherwise, delete the element pointed by the iter
@@ -103,20 +173,49 @@ public:
 	 *  or nothing if the list after the operation
 	 *  don't contain 2nd elememt.
 	 */
-	iterator erase(iterator pos) {}
+	iterator erase(iterator pos) {
+		if (pos.p == nullptr || pos.p == head || pos.p == tail)
+			return pos;
+		node* to_delete = pos.p;
+		pos.p = pos.p->next;
+		to_delete->prev->next = to_delete->next;
+		to_delete->next->prev = to_delete->prev;
+		delete to_delete;
+		return pos;
+	}
 
 	/**
 	 * the following are operations of double list
 	 */
-	void insert_head(const T &val) {}
-	void insert_tail(const T &val) {}
-	void delete_head() {}
-	void delete_tail() {}
+	void insert_head(const T &val) {
+		node* tmp = new node(val, head, head->next);
+		head->next->prev = tmp;
+		head->next = tmp;
+	}
+	void insert_tail(const T &val) {
+		node* tmp = new node(val, tail->prev, tail);
+		tail->prev->next = tmp;
+		tail->prev = tmp;
+	}
+	void delete_head() {
+		node* tmp = head->next;
+		head->next = tmp->next;
+		tmp->next->prev = head;
+		delete tmp;
+	}
+	void delete_tail() {
+		node* tmp = tail->prev;
+		tail->prev = tmp->prev;
+		tmp->prev->next = tail;
+		delete tmp;
+	}
 	/**
 	 * if didn't contain anything, return true,
 	 * otherwise false.
 	 */
-	bool empty() {}
+	bool empty()  const {
+		return head->next == tail;
+	}
 };
 
 template<class Key, class T, class Hash = std::hash<Key>, class Equal = std::equal_to<Key>>
